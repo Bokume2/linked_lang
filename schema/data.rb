08973ff7bec1f57adbae2c7335165132d1414b61
Bokume2 <nagatomoyuu2@gmail.com>
2025-06-15 23:13:10 +0900

    add: プログラム内データを表すclassを追加

---
+++ schema/data.rb
@@ -0,0 +1,50 @@
+module LinkedLang
+  class Data
+    def initialize(value)
+      @value = value
+    end
+
+    def exec(machineContext)
+      @value
+    end
+
+    attr_reader :value
+  end
+
+  class Number < Data
+    def initialize(value)
+      unless value.is_a?(Integer) || value.is_a?(Float)
+        raise ArgumentError
+      end
+      super value
+    end
+  end
+
+  class String < Data
+    def initialize(value)
+      unless value.is_a?(::String)
+        raise ArgumentError
+      end
+      super value
+    end
+  end
+
+  class Boolean < Data
+    def initialize(value)
+      super(
+        case value
+        when "true"
+          true
+        when "false"
+          false
+        when true
+          true
+        when false
+          false
+        else
+          raise ArgumentError
+        end
+      )
+    end
+  end
+end
