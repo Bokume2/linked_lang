d88e4d6d08514519d61983d0c6762e52c9f75882
Bokume2 <nagatomoyuu2@gmail.com>
2025-06-27 16:46:49 +0900

    fix: 数値以外の大小比較を禁止

--- schema/comparation.rb
+++ schema/comparation.rb
@@ -4,22 +4,22 @@ module LinkedLang
   class ComparationOperation < BinaryLogicalOperation
     protected
 
-    def evalWithBlock(machineContext, &block)
+    def evalWithBlock(machineContext, cls, &block)
       actual_lop, actual_rop = @lop.eval(machineContext), @rop.eval(machineContext)
-      raise TypeError unless (actual_lop.class == actual_rop.class) && actual_lop.is_a?(Data)
+      raise TypeError unless (actual_lop.class == actual_rop.class) && actual_lop.is_a?(cls)
       Boolean.new(yield(actual_lop.value, actual_rop.value))
     end
   end
 
   class Equal < ComparationOperation
     def eval(machineContext)
-      evalWithBlock(machineContext) {|l, r| l == r}
+      evalWithBlock(machineContext, Data) {|l, r| l == r}
     end
   end
 
   class LessThan < ComparationOperation
     def eval(machineContext)
-      evalWithBlock(machineContext) {|l, r| l < r}
+      evalWithBlock(machineContext, Number) {|l, r| l < r}
     end
   end
 end
