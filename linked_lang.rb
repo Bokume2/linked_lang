fd5d4c33be2f2b01154c8697f8aac5381c0dba9a
Bokume2 <nagatomoyuu2@gmail.com>
2025-07-01 12:46:55 +0900

    fix: 処理系に渡すコマンドライン引数のバリデーションを微修正

--- linked_lang.rb
+++ linked_lang.rb
@@ -4,8 +4,10 @@ require_relative "parser"
 require_relative "machine_context"
 
 if __FILE__ == $0
-  if ARGV.length != 1
+  if ARGV.length < 1
     raise "No source file name."
+  elsif ARGV.length > 1
+    raise "Unnecessary arguments."
   end
   LinkedLang::MachineContext.new(LinkedLang::Parser.parse(ARGF.read)).run
 end
